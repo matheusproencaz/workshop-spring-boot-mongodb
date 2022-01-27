4c4492d8dd624ab1e8407dfcc48f803297079950
Matheus Proenca <matheusproencaz@gmail.com>
2022-01-27 14:17:57 -0300

    FindByID com tratamento de excessões

--- src/main/java/com/mpz/workshopmongodb/services/UserService.java
+++ src/main/java/com/mpz/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mpz.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mpz.workshopmongodb.domain.User;
 import com.mpz.workshopmongodb.repositories.UserRepository;
+import com.mpz.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService{
@@ -16,5 +18,10 @@ public class UserService{
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
